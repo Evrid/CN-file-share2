a6dfc3c1854e9af04483059d556bc94665583bb3
Kevin li <talentsgenius@outlook.com>
2023-08-27 17:47:54 +0200

    Now we can save document to usersavedinfo table (as user favorite).

--- StudentFileShare6/Controllers/UserSavedInfoController.cs
+++ StudentFileShare6/Controllers/UserSavedInfoController.cs
@@ -14,13 +14,15 @@ namespace StudentFileShare6.Controllers
         private readonly UserSavedInfoContext _context;
         private readonly CourseContext _courseContext;
         private readonly UniversityContext _universityContext;
+        private readonly DocumentContext _documentContext;
 
-        public UserSavedInfoController(UserManager<IdentityUser> userManager, UserSavedInfoContext context, CourseContext courseContext, UniversityContext universityContext)
+        public UserSavedInfoController(UserManager<IdentityUser> userManager, UserSavedInfoContext context, CourseContext courseContext, UniversityContext universityContext, DocumentContext documentContext)
         {
             _userManager = userManager;
             _context = context;
             _courseContext = courseContext;
             _universityContext = universityContext;
+            _documentContext = documentContext;
         }
 
         //public async Task<IActionResult> UserSaved()
@@ -51,7 +53,7 @@ namespace StudentFileShare6.Controllers
             if (existingSavedCourse != null)
             {
                 // Course is already saved
-                return Json(new { success = false, message = "Course already saved" });
+                return Json(new { success = false, message = "课程已保存" });
             }
 
             var courseToSave = await _courseContext.Course
@@ -61,7 +63,7 @@ namespace StudentFileShare6.Controllers
             if (courseToSave == null)
             {
                 // Course not found
-                return Json(new { success = false,  message = "Course not found" });
+                return Json(new { success = false,  message = "找不到课程" });
             }
 
             var userSavedInfo = new UserSavedInfo
@@ -74,7 +76,7 @@ namespace StudentFileShare6.Controllers
             _context.UserSavedInfo.Add(userSavedInfo);
             await _context.SaveChangesAsync();
 
-            return Json(new { success = false, message = "Course saved" });
+            return Json(new { success = false, message = "课程保存成功" });
         }
 
 
@@ -92,7 +94,7 @@ namespace StudentFileShare6.Controllers
             if (existingSavedUniversity != null)
             {
                 // Course is already saved
-                return Json(new { success = false, message = "University already saved" });
+                return Json(new { success = false, message = "大学已保存" });
             }
 
             var universityToSave = await _universityContext.Universities
@@ -102,7 +104,7 @@ namespace StudentFileShare6.Controllers
             if (universityToSave == null)
             {
                 // Course not found
-                return Json(new { success = false, message = "University not found" });
+                return Json(new { success = false, message = "未找到大学" });
             }
 
             var userSavedInfo = new UserSavedInfo
@@ -115,7 +117,48 @@ namespace StudentFileShare6.Controllers
             _context.UserSavedInfo.Add(userSavedInfo);
             await _context.SaveChangesAsync();
 
-            return Json(new { success = false, message = "University saved" });
+            return Json(new { success = false, message = "大学保存成功" });
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SaveDocument(string documentId)
+        {
+            var userId = _userManager.GetUserId(User);  // Get current logged-in user's id
+
+            // First, check if the document is already saved by the user
+            var existingSavedDocument = await _context.UserSavedInfo
+                .Where(u => u.UserId == userId && u.DocumentID == documentId)
+                .FirstOrDefaultAsync();
+
+            if (existingSavedDocument != null)
+            {
+                // Document is already saved
+                return Json(new { success = false, message = "文档已保存" });
+            }
+
+            var documentToSave = await _documentContext.Document
+                .Where(d => d.DocumentID == documentId)
+                .FirstOrDefaultAsync();
+
+            if (documentToSave == null)
+            {
+                // Document not found
+                return Json(new { success = false, message = "未找到文档" });
+            }
+
+            var userSavedInfo = new UserSavedInfo
+            {
+                UserId = userId,
+                DocumentID = documentToSave.DocumentID,
+                DocumentName = documentToSave.Name
+            };
+
+            _context.UserSavedInfo.Add(userSavedInfo);
+            await _context.SaveChangesAsync();
+
+            return Json(new { success = true, message = "文档保存成功" });
+        }
+
     }
 }
