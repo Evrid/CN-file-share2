55f4bce69cef94e4802fa5d36ef45969c5db9b4e
Kevin li <talentsgenius@outlook.com>
2023-09-03 12:00:22 +0200

    Now create new course work mostly well.

--- StudentFileShare6/wwwroot/js/site.js
+++ StudentFileShare6/wwwroot/js/site.js
@@ -4,4 +4,41 @@
 // Write your JavaScript code.
 
 
+function getUniversities(searchText) {
+    fetch(`/Document/GetUniversities?searchText=${searchText}`)
+        .then(response => response.json())
+        .then(data => {
+            customDatalist.innerHTML = '';
+            customDatalist.style.display = 'block';
 
+            if (Object.keys(data).length === 0) {
+                // Show 'No matching university' if no universities are found
+                const divElement = document.createElement('div');
+                divElement.textContent = '没有匹配的大学';
+                divElement.classList.add('custom-option');
+                customDatalist.appendChild(divElement);
+            } else {
+                // Populate the dropdown with universities
+                for (const key in data) {
+                    if (data.hasOwnProperty(key)) {
+                        const university = data[key];
+                        const divElement = document.createElement('div');
+                        divElement.textContent = university.name;
+                        divElement.classList.add('custom-option');
+                        divElement.setAttribute('data-value', university.schoolID);
+                        customDatalist.appendChild(divElement);
+                    }
+                }
+
+                // Add click event listeners to each custom option
+                document.querySelectorAll('.custom-option').forEach(option => {
+                    option.addEventListener('click', function () {
+                        const selectedValue = this.getAttribute('data-value');
+                        inputElement.value = this.textContent;
+                        hiddenInput.value = selectedValue;
+                        customDatalist.style.display = 'none';
+                    });
+                });
+            }
+        });
+}
